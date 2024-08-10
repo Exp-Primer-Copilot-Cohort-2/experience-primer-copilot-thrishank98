753948d7630ea265e8914ae7f09eb9e978a076e0
KALLUR THRISHANK <521146@student.nitandhra.ac.in>
2024-08-10 18:07:45 +0000

    Copilot thrid commit

--- comments.js
+++ comments.js
@@ -1,27 +1,6 @@
 // create a web server that listens on port 3000
-// create a route that listens for POST requests to /comments
-// when a POST request is made, parse the incoming form data and log it to the console
-// test your server using curl or Postman
-
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.post('/comments', (req, res) => {
-  console.log(req.body);
-  res.send('Thanks for your comments!');
-});
-
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
-
-// curl -X POST -d "name=John&comment=Hello" http://localhost:3000/comments
-// { name: 'John', comment: 'Hello' }
-// Thanks for your comments!
-
-// curl -X POST -d "name=Jane&comment=Hi" http://localhost:3000/comments
-// { name: 'Jane', comment: 'Hi' }
-// Thanks for your comments!
+// 1. create a route for /comments
+// 2. create a route for /comments/new
+// 3. create a route for /comments/:id
+// 4. create a route for /comments/:id/edit
+// 5. create a route for /comments/:id/delete
